e5fd16873ec0ec7c079a8301d19c0c62804d5ba0
Giulia Correa <giuliarafaela32@gmail.com>
2025-10-29 11:56:27 -0400

    Menu do administrador aparecendo mesmo quando da f5

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,32 +1,26 @@
-// src/context/AuthContext.tsx
 import React, { createContext, useContext, useState, useEffect } from 'react';
-//import jwtDecode from 'jwt-decode';
 import { decodeToken } from '../utils/jwtDecodeHelper';
-
 
 interface AuthContextType {
   isAuthenticated: boolean;
-  login: (cpf: string, password: string, administrador: boolean, nome: string, email: string) => void;
+  login: (token: string, cpf: string, administrador: boolean, nome: string, email: string) => void;
   logout: () => void;
   cpf: string | null;
   token: string | null;
-  administrador: boolean | null; 
+  administrador: boolean | null;
   nome: string | null;
   email: string | null;
-
 }
 
-
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
-
 
 const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [cpf, setCpf] = useState<string | null>(null);
   const [token, setToken] = useState<string | null>(null);
-  const [administrador, setAdministrador] = useState<string | null>(null);
+  const [administrador, setAdministrador] = useState<boolean | null>(null);
   const [nome, setNome] = useState<string | null>(null);
-  const [email, setEmail ] = useState<string | null>(null);
+  const [email, setEmail] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -36,18 +30,17 @@ const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => 
     const storedNome = localStorage.getItem('nome');
     const storedEmail = localStorage.getItem('email');
 
-    //manter logado mesmo após refresh
     if (storedToken && storedCpf && storedAdministrador) {
       setToken(storedToken);
       setCpf(storedCpf);
-      setAdministrador(storedAdministrador);
+      setAdministrador(storedAdministrador === 'true');
       setNome(storedNome);
       setEmail(storedEmail);
       setIsAuthenticated(true);
     }
+
     setIsLoading(false);
   }, []);
-
 
   useEffect(() => {
     const checkToken = () => {
@@ -63,44 +56,42 @@ const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => 
         }
       }
     };
-    
+
     const interval = setInterval(checkToken, 10000);
     return () => clearInterval(interval);
   }, [token]);
 
+  const login = (token: string, cpf: string, administrador: boolean, nome: string, email: string) => {
+    localStorage.setItem('token', token);
+    localStorage.setItem('cpf', cpf);
+    localStorage.setItem('administrador', String(administrador));
+    localStorage.setItem('nome', nome);
+    localStorage.setItem('email', email);
 
-    const login = (token: string, cpf: string, administrador: string, nome: string, email: string) => {
-      localStorage.setItem('token', token);
-      localStorage.setItem('cpf', cpf);
-      localStorage.setItem('administrador', administrador); 
-      localStorage.setItem('nome', nome);
-      localStorage.setItem('email', email);
-
-
-      setToken(token);
-      setCpf(cpf);
-      setAdministrador(administrador);
-      setNome(nome);
-      setEmail(email);
-      setIsAuthenticated(true);
+    setToken(token);
+    setCpf(cpf);
+    setAdministrador(administrador);
+    setNome(nome);
+    setEmail(email);
+    setIsAuthenticated(true);
   };
 
-      const logout = () => {
-      localStorage.removeItem('token');
-      localStorage.removeItem('cpf');
-      localStorage.removeItem('administrador');
-      localStorage.removeItem('nome');
-      localStorage.removeItem('email');
+  const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('cpf');
+    localStorage.removeItem('administrador');
+    localStorage.removeItem('nome');
+    localStorage.removeItem('email');
 
-      setToken(null);
-      setCpf(null);
-      setAdministrador(null);
-      setNome(null);
-      setEmail(null);
-      setIsAuthenticated(false);
+    setToken(null);
+    setCpf(null);
+    setAdministrador(null);
+    setNome(null);
+    setEmail(null);
+    setIsAuthenticated(false);
   };
 
-    return (
+  return (
     <AuthContext.Provider
       value={{ isAuthenticated, login, logout, cpf, token, administrador, nome, email }}
     >
