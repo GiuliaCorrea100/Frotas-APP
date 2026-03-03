15098783b1b6cd77e7d791dce70368ead503963c
Giulia Correa <giuliarafaela32@gmail.com>
2026-03-03 09:07:42 -0400

    Botao de APROVAR comprovante e REPROVAR comprovante; adicionados no modal de comprovante de boleto.

--- src/pages/administrador/multas/ModalComprovanteBoleto.tsx
+++ src/pages/administrador/multas/ModalComprovanteBoleto.tsx
@@ -6,7 +6,8 @@ import {
   DialogActions,
   Button,
   Box,
-  Typography
+  Typography,
+  Divider
 } from "@mui/material";
 import { MultaDto, MultaService } from "../../../services/MultaService";
 
@@ -43,15 +44,14 @@ export default function ModalComprovanteMulta({ open, onClose, multa }: Props) {
       <DialogTitle>Comprovante de Pagamento</DialogTitle>
 
       <DialogContent>
-        <Typography>
+        <Typography sx={{ mb: 2 }}>
           Escolha o que deseja fazer com o comprovante desta multa.
         </Typography>
-      </DialogContent>
 
-      <DialogActions>
-        <Box sx={{ display: "flex", gap: 2, px: 2, pb: 2 }}>
+        <Box sx={{ display: "flex", gap: 2 }}>
           <Button
             variant="outlined"
+            fullWidth
             onClick={handleVisualizar}
           >
             Visualizar boleto
@@ -59,9 +59,34 @@ export default function ModalComprovanteMulta({ open, onClose, multa }: Props) {
 
           <Button
             variant="contained"
+            fullWidth
             onClick={handleDownload}
           >
             Baixar boleto
+          </Button>
+        </Box>
+      </DialogContent>
+
+      <Divider sx={{ my: 1 }} />
+
+      <DialogActions sx={{ px: 3, pb: 2 }}>
+        <Box sx={{ display: "flex", gap: 2, width: "100%" }}>
+          <Button
+            variant="contained"
+            color="success"
+            fullWidth
+            onClick={() => {}}
+          >
+            Aprovar comprovante
+          </Button>
+
+          <Button
+            variant="contained"
+            color="error"
+            fullWidth
+            onClick={() => {}}
+          >
+            Reprovar comprovante
           </Button>
         </Box>
       </DialogActions>
