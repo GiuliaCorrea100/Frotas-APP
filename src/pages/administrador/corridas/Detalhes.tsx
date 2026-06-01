983a9d9a2b62d625ecdb26f81a9a10dcf12141c8
unknown <Administrador@T117678-CDSIS.unir.br>
2026-06-01 10:38:17 -0400

    botão de exportar relatório

--- src/pages/administrador/corridas/Detalhes.tsx
+++ src/pages/administrador/corridas/Detalhes.tsx
@@ -44,6 +44,7 @@ import EdicaoPercursosModal from "./modais/ModalEdicaoPercurso";
 import CadastrarPercursosModal from "./modais/ModalCadastroPercurso";
 import AppLayout from "../../../components/Layout";
 import { formatDate, formatDateOnly } from "../../../utils/formatDate";
+import ExportarCorridaPDF from "./ExportarRelatorioDetalhes";
 
 const DetalhesRequisicao: React.FC = () => {
   const theme = useTheme();
@@ -147,7 +148,7 @@ const DetalhesRequisicao: React.FC = () => {
     {
       field: "descricao",
       headerName: "Descrição",
-      flex: 1,
+      width: 300,
       renderCell: (params) => (
         <Typography color="text.primary">{params.value}</Typography>
       ),
@@ -155,7 +156,7 @@ const DetalhesRequisicao: React.FC = () => {
     {
       field: "dataOcorrencia",
       headerName: "Data",
-      flex: 1,
+      width: 200,
       renderCell: (params) => (
         <Typography color="text.primary">
           {formatDateOnly(params.value)}
@@ -230,7 +231,7 @@ const DetalhesRequisicao: React.FC = () => {
     {
       field: "nomeTipoCombustivel",
       headerName: "Combustível",
-      flex: 1,
+      width: 200,
       renderCell: (params) => (
         <Typography color="text.primary">{params.value}</Typography>
       ),
@@ -238,7 +239,7 @@ const DetalhesRequisicao: React.FC = () => {
     {
       field: "quantidade",
       headerName: "Quantidade de Litros",
-      flex: 1,
+      width: 100,
       renderCell: (params) => {
         const value = Number(params.value);
         return isNaN(value) ? "-" : value.toFixed(2);
@@ -624,15 +625,33 @@ const DetalhesRequisicao: React.FC = () => {
           }}
         >
           <Box ml={1.5}>
-            <Typography
-              variant="h6"
-              fontWeight="bold"
-              color="text.primary"
-              ml={1.5}
-              pt={2}
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                mr: 3,
+                pt: 2,
+              }}
             >
-              Informações Básicas
-            </Typography>
+              <Typography
+                variant="h6"
+                fontWeight="bold"
+                color="text.primary"
+                ml={1.5}
+              >
+                Informações Básicas
+              </Typography>
+              
+              <ExportarCorridaPDF
+                corrida={corrida}
+                ocorrencias={ocorrencias}
+                abastecimentos={abastecimentos}
+                percursos={percursos}
+                disabled={loading || !corrida}
+              />
+            </Box>
+            
             <CardContent>
               {loading ? (
                 <Typography variant="body2" color="text.secondary">
